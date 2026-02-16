b0d2f031b250609c6bae858e251e32ef2e60cfe3
Pavel Gabriel <alovak@gmail.com>
2026-02-16 23:11:18 +0100

    se run with version 0.0.0-dev.0

--- src/SDKConfiguration.php
+++ src/SDKConfiguration.php
@@ -22,13 +22,13 @@ class SDKConfiguration
 
     public string $language = 'php';
 
-    public string $openapiDocVersion = 'latest';
+    public string $openapiDocVersion = 'dev';
 
-    public string $sdkVersion = '0.7.45';
+    public string $sdkVersion = '0.0.0-dev.0';
 
-    public string $genVersion = '2.823.1';
+    public string $genVersion = '2.824.1';
 
-    public string $userAgent = 'speakeasy-sdk/php 0.7.45 2.823.1 latest moov/moov-php';
+    public string $userAgent = 'speakeasy-sdk/php 0.0.0-dev.0 2.824.1 dev moov/moov-php';
     /** @var array<string, array<string, array<string, mixed>>> */
     public ?array $globals = [
         'parameters' => [],
